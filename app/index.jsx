2934633bce9e31749f695b9d961f2dd5c2f78d2a
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-12-17 12:12:14 +0000

    init

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+	return (
+		<View>
+			<Text className={"text-4xl text-blue-500"}>Hello World</Text>
+		</View>
+	);
+}
