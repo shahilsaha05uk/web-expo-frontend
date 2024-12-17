8dd9f1915ae0ac27f6dec6ee03dd60a473484bc5
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-12-17 16:40:23 +0000

    changes

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,16 @@
+import {
+  Button,
+  ButtonText,
+} from '@/components/ui/button';
 import { Text, View } from "react-native";
 
 export default function App() {
 	return (
 		<View>
-			<Text className={"text-4xl text-blue-500"}>Hello World</Text>
+      <Text className={"text-4xl text-red-500 font-bold m-8"}>Hello World</Text>
+      <Button className='bg-error-500'>
+        <ButtonText>Press me</ButtonText>
+      </Button>
 		</View>
 	);
 }
